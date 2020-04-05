7893e4738edbc6166618cd6b5540d1b96f938472
kmok <kmok@thoughtworks.com>
2020-04-05 15:46:21 +0200

    km: cleaning up old files

--- iam-admin/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "myteam-developer-create-role-boundary-arn" {
-  value = "${module.myteam-admin.myteam-developer-create-role-boundary-arn}"
-}
-
-output "myteam-developer-create-user-boundary-arn" {
-  value = "${module.myteam-admin.myteam-developer-create-user-boundary-arn}"
-}
--- iam-admin/role-myteam-readonly/policy-readonly-aws-read-only.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_policy_attachment" "readonly_access_read_only_policy_attachment" {
-  name       = "${var.team-name}-${var.role}-readonly_access_read_only_policy_attachment"
-  roles      = ["${aws_iam_role.role.name}"]
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
